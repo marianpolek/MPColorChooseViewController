fd9304f086aa872aa06f996a9cfe5df54bc88e3b
marianpolek <marian.polek@gmail.com>
2013-07-07 23:45:51 +0200

    Update MPColorChooseViewController.m

--- MPColorChooseViewController.m
+++ MPColorChooseViewController.m
@@ -26,7 +26,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-  // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.
     
     [self.view setBackgroundColor:[UIColor whiteColor]];
     
@@ -39,9 +39,53 @@
         [self.view addSubview:slider];
     }
     
+    UIButton *down = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [down setTitle:@"<" forState:UIControlStateNormal];
+    [down setFrame:CGRectMake(10, 135, 30, 30)];
+    [down addTarget:self action:@selector(actionDown:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:down];
+    
+    UIButton *up = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [up setTitle:@">" forState:UIControlStateNormal];
+    [up setFrame:CGRectMake(60, 135, 30, 30)];
+    [up addTarget:self action:@selector(actionUp:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:up];
+    
     label = [[UILabel alloc] initWithFrame:CGRectMake(10, 100, 230, 30)];
     [label setBackgroundColor:[UIColor clearColor]];
     [self.view addSubview:label];
+}
+
+-(void)actionDown:(id)sender{
+    if(colorR == 0 || colorG == 0 || colorB == 0)
+        return;
+    
+    colorR--;
+    colorG--;
+    colorB--;
+    
+    [((UISlider*)[self.view viewWithTag:100]) setValue:colorR];
+    [((UISlider*)[self.view viewWithTag:101]) setValue:colorG];
+    [((UISlider*)[self.view viewWithTag:102]) setValue:colorB];
+
+    [self changeWasDid];
+
+}
+
+-(void)actionUp:(id)sender{
+    
+    if(colorR == 255 || colorG == 255 || colorB == 255)
+        return;
+    
+    colorR++;
+    colorG++;
+    colorB++;
+    
+    [((UISlider*)[self.view viewWithTag:100]) setValue:colorR];
+    [((UISlider*)[self.view viewWithTag:101]) setValue:colorG];
+    [((UISlider*)[self.view viewWithTag:102]) setValue:colorB];
+    
+    [self changeWasDid];
 }
 
 -(void)colorChanged:(id)sender{
@@ -61,6 +105,11 @@
         default:
             break;
     }
+    
+    [self changeWasDid];
+}
+
+-(void)changeWasDid{
     
     if(_delegate != nil){
         [_delegate colorDidChanged:[UIColor colorWithRed:colorR/255.0f green:colorG/255.0f blue:colorB/255.0f alpha:1]];
